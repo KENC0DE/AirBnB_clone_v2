06aad4d39dce2a627d5359f884cd2e974d7deb35
KENC0DE <kenansameseret@gmail.com>
2023-12-07 19:37:13 +0530

    config

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Web static Deployment automation.
+
+if [ ! dpkg -s nginx &> /dev/null ]
+then
+        sudo apt-get update -y
+        sudo upt-get install nginx -y
+fi
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo echo "<!DOCTYPE html>
+<html>
+        <head>
+                <title>Faker</title>
+        </head>
+        <body>
+                <p1>I am Fake</p1>
+        </body>
+</html>" > /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo service nginx restart
