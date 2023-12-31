6f4967aa9e0e1b1723ee38384eb4018a355e7721
KENC0DE <kenansameseret@gmail.com>
2024-01-01 03:33:52 +0530

    flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+    Web flask 04
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Return 'hello hbnb' """
+    return 'Hellow HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Return 'hbnb' """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def about_c(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def about_python(text='is cool'):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def is_number(n):
+    try:
+        num = int(n)
+        return f'{n} is a number'
+    except Exception:
+        pass
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
