ae27560666c2db46ff49c10310d498707f857a36
KENC0DE <kenansameseret@gmail.com>
2024-01-01 03:10:51 +0530

    flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Web flask 02
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Return 'hello hbnb' """
+    return 'Hellow HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Return 'hbnb' """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def about_c(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
